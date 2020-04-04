d16cbb3b1857767088ef1257f2d3e7590c9a10bd
Jkutkut <jorgeregonzalez@gmail.com>
2020-04-04 17:02:56 +0200

    Prueba2 funciona

--- Interfaces/PY_NuevoCliente/prueba2.py
+++ Interfaces/PY_NuevoCliente/prueba2.py
@@ -10,5 +10,12 @@ def helloCallBack():
    msg = messagebox.showinfo( "Hello Python", "Hello World")
 
 B = Button(top, text = "Hello", command = helloCallBack)
-B.place(x = 50,y = 50)
+B.place(x = 50,y = 100)
+
+
+L1 = Label(top, text = "Hola caracola")
+L1.place(x = 50, y = 50)
+E1 = Entry(top, bd = 5)
+E1.place(x = 50, y = 150)
+
 top.mainloop()
