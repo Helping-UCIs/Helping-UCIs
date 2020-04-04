ab2f2baa0331cd90af84eab1ec9f9c8b8eb18daf
Jkutkut <jorgeregonzalez@gmail.com>
2020-04-04 16:49:09 +0200

    Hello world funciona

--- Interfaces/PY_NuevoCliente/prueba2.py
+++ Interfaces/PY_NuevoCliente/prueba2.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
-import tkinter # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
-top = tkinter.Tk()
+from tkinter import * # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
+from tkinter import messagebox
+top = Tk()
 # Code to add widgets will go here...
+
+top.geometry("100x100")
+def helloCallBack():
+   msg = messagebox.showinfo( "Hello Python", "Hello World")
+
+B = Button(top, text = "Hello", command = helloCallBack)
+B.place(x = 50,y = 50)
 top.mainloop()
